cf0db4c578e8b1c6b170cb8c9fae3201a02ac01d
Veronica Pagini <veronica.pagini@outlook.com>
2015-07-30 13:51:47 +0200

    should work :D

--- platforms/android/assets/www/js/configApp.js
+++ platforms/android/assets/www/js/configApp.js
@@ -1,12 +1,31 @@
+//    var num = 0;
+//    var c = 0;
+//    var color = null;
+//    var f = 0;
+//    var font = null;
+
     function onDeviceReady() {
         set();  
     };
     function set(){
+//        if (num!=0) {
+//            read();
+//        }
+//        if (c==0){
+//            setcol();    
+//        }
+//        if (f==0){
+//            setFCSS();    
+//        }
         read();
         var btnhome = document.getElementById("btnhome");
         btnhome.addEventListener("click", home);
         var imp = document.getElementById("submit");
         submit.addEventListener("click", write);
+        var backcol = document.getElementById("backcol");
+        backcol.addEventListener("click", setcolor);
+        var fontApp = document.getElementById("font");
+        fontApp.addEventListener("click", setfont);
     }
     function home(){
         window.location="index.html";
@@ -17,6 +36,7 @@
             fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, function(fileEntry){
                 fileEntry.createWriter(function(writer){
                     writer.onwriteend = function(evt) {
+//                        num = writer.length;
                         read();
                     };
                     writer.write(document.getElementById("initpage").value);
@@ -43,4 +63,36 @@
 
     function fail(e) {
         alert("Errore!");
+    }
+//    function setBack(){
+//        c = 1;
+//        color = 'azure'
+//        document.body.style.backgroundColor = color;
+//        setcolor();
+//    }
+//    function setFont(){
+//        f = 1;
+//        font = 'Arial, sans-serif'
+//        document.body.style.fontFamily = font;
+//        setfont();
+//    }
+    function setfont(){  
+        document.body.style.fontFamily = 'Verdana, sans-serif'
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+            fileSystem.root.getFile("fontCSS.txt", {create: true, exclusive: false}, function(fileEntry){
+                fileEntry.createWriter(function(writer){
+                    writer.write('Verdana, sans-serif');
+                }, fail);
+            }, fail);
+        }, fail);
+    }
+    function setcolor(){  
+        document.body.style.backgroundColor = 'darksalmon';
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+            fileSystem.root.getFile("backCSS.txt", {create: true, exclusive: false}, function(fileEntry){
+                fileEntry.createWriter(function(writer){
+                    writer.write('darksalmon');
+                }, fail);
+            }, fail);
+        }, fail);
     }
