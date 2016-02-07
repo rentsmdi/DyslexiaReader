6397fa69f8eac4f4ce14b9656c906a3ac18d8a54
Veronica Pagini <veronica.pagini@outlook.com>
2016-02-07 15:02:14 +0100

    dovrebbe funzionare tutto

--- www aggiornato/js/impostazioni.js
+++ www aggiornato/js/impostazioni.js
@@ -1,9 +1,7 @@
 var filecss = null;
 var variables = null;
 
-var fileConn = "conn07.txt";
-
-var c = null;
+var fileConn = "conn08.txt";
 
 var selValue = null;
 var textValue = null;
@@ -17,7 +15,7 @@ var intlinea = null;
 var wspc = null;
 var lspc = null;
 
-var CSScode = 'cssCode2.txt';
+var CSScode = 'cssCode3.txt';
 
 // FUNZIONI
 function onDeviceReady() {
@@ -45,10 +43,10 @@ function set() {
         window.location="index.html";
     }, false);
 }
-// LETTURA CSS PER DIV
+// LETTURA VARIABILI "VISUALIZZAZIONE CONTENUTI WEB"
 function loadCSS(){
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-        fileSystem.root.getFile("variables.txt", null, function(fileEntry){
+        fileSystem.root.getFile("variables1.txt", null, function(fileEntry){
             fileEntry.file(function(file) {
             var reader = new FileReader();
             reader.onloadend = function(e) {
@@ -56,10 +54,7 @@ function loadCSS(){
             }
             reader.readAsText(file);
             });
-        }, function(e){ 
-            //se non è stata impostata si viene riderizionati alle impostazioni
-            alert('Non è presente una configurazione personalizzata!');
-        });
+        }, function(e){ });
     });
 }
 function setCSS(css){
@@ -119,72 +114,6 @@ function setCSS(css){
     document.getElementById("testsettings").style.letterSpacing = lspc;
 }
 
-// IMPOSTAZIONI APPLICAZIONE
-function list(event){
-    event.preventDefault();
-    selValue = event.target.value;
-    setcolor();
-}
-function textapp(event){
-    event.preventDefault();
-    textValue = event.target.value;
-    if (textValue == '0')
-        textValue = "'Roboto', sans-serif";
-    if (textValue == '1')
-        textValue = "'OpenDyslexic'";
-    if (textValue == '2')
-        textValue = "'Chelsea Market', cursive";
-    if (textValue == '3')
-        textValue = "'Slackey', cursive";
-    if (textValue == '4')
-        textValue = "'Open Sans', sans-serif";
-    if (textValue == '5')
-        textValue = "'Exo', sans-serif";
-    if (textValue == '6')
-        textValue = "'Ubuntu', sans-serif";
-    setfont();
-}
-function size(event){
-    event.preventDefault();
-    sizeValue = event.target.value;
-//    if (sizeValue == 'Piccolo')
-//        sizeValue = "small";
-//    if (sizeValue == 'Normale')
-//        sizeValue = "medium";
-//    if (sizeValue == 'Grande')
-//        sizeValue = "x-large";
-    setsize();
-};
-
-//SALVATAGGIO IMPOSTAZIONI APPLICAZIONE
-function setfont(){  
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-        fileSystem.root.getFile("fontCSS1.txt", {create: true, exclusive: false}, function(fileEntry){
-            fileEntry.createWriter(function(writer){
-                writer.write(textValue);
-            });
-        });
-    });
-};
-function setcolor(){  
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-        fileSystem.root.getFile("backCSS1.txt", {create: true, exclusive: false}, function(fileEntry){
-            fileEntry.createWriter(function(writer){
-                writer.write(selValue);
-            });
-        });
-    });
-};
-function setsize(){  
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-        fileSystem.root.getFile("fontsizeCSS1.txt", {create: true, exclusive: false}, function(fileEntry){
-            fileEntry.createWriter(function(writer){
-                writer.write(sizeValue);
-            });
-        });
-    });
-};
-
 //IMPOSTAZIONI URL PAGINA INIZIALE
 function writeURL(){
     alert("Le impostazioni sono state salvate con successo!"); 
@@ -194,11 +123,8 @@ function writeURL(){
                 writer.onwriteend = function(evt) {
                     window.location = "impostazioni.html";
                 };
-                if ((document.getElementById("initpage").value.length)<= 0){
-//                    alert("Errore nell'impostazione della Pagina Iniziale.\nPer favore, inserire un link non vuoto.");
-//                    window.location = "impostazioni.html";
+                if ((document.getElementById("initpage").value.length)<= 0)
                     writer.write("www.google.it");
-                }
                 else 
                     writer.write(document.getElementById("initpage").value);
             });
@@ -220,26 +146,104 @@ function setURL(fileEntry) {
     });
 }
 
-//LETTURA DA FILE E APPLICAZIONE DELLE IMPOSTAZIONI APPLICAZIONE
+//IMPOSTAZIONI APPLICAZIONE
+function list(event){
+    event.preventDefault();
+    selValue = event.target.value;
+    if(selValue=='Standard'){
+        document.getElementsByClassName("bg1")[0].style.backgroundColor = '#fffff9';  
+        document.getElementsByClassName("bg2")[0].style.backgroundColor = '#fffa8f'; 
+    }
+    else if(selValue=='Greyscale'){
+        document.getElementsByClassName("bg1")[0].style.backgroundColor = '#ffffff';
+        document.getElementsByClassName("bg2")[0].style.backgroundColor = '#f2f2f2'; 
+    }
+    else if(selValue=='Lavender'){
+        document.getElementsByClassName("bg1")[0].style.backgroundColor = '#d8ffe8';
+        document.getElementsByClassName("bg2")[0].style.backgroundColor = '#ebd6ff';
+    }
+}
+function textapp(event){
+    event.preventDefault();
+    textValue = event.target.value;
+    if (textValue == '0')
+        textValue = "'Roboto', sans-serif";
+    if (textValue == '1')
+        textValue = "'OpenDyslexic'";
+    if (textValue == '2')
+        textValue = "'Chelsea Market', cursive";
+    if (textValue == '3')
+        textValue = "'Slackey', cursive";
+    if (textValue == '4')
+        textValue = "'Open Sans', sans-serif";
+    if (textValue == '5')
+        textValue = "'Exo', sans-serif";
+    if (textValue == '6')
+        textValue = "'Ubuntu', sans-serif";
+    document.body.style.fontFamily = textValue;
+}
+function size(event){
+    event.preventDefault();
+    sizeValue = event.target.value;
+    document.body.style.fontSize = sizeValue;
+};
+function saveAll(){
+    setcolor();
+    setfont()
+    setsize();
+}
+
+//SALVATAGGIO IMPOSTAZIONI APPLICAZIONE
+function setcolor(){  
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+        fileSystem.root.getFile("backCSS2.txt", {create: true, exclusive: false}, function(fileEntry){
+            fileEntry.createWriter(function(writer){
+                writer.write(selValue);
+            });
+        });
+    });
+};
+function setfont(){  
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+        fileSystem.root.getFile("fontCSS2.txt", {create: true, exclusive: false}, function(fileEntry){
+            fileEntry.createWriter(function(writer){
+                writer.write(textValue);
+            });
+        });
+    });
+};
+function setsize(){  
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+        fileSystem.root.getFile("fontsizeCSS2.txt", {create: true, exclusive: false}, function(fileEntry){
+            fileEntry.createWriter(function(writer){
+                writer.write(sizeValue);
+            });
+        });
+    });
+};
+
+
+//LETTURA DA FILE E APPLICAZIONE DELLE IMPOSTAZIONI APPLICAZIONE (no pagina iniziale)
 function readBack(){
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-        fileSystem.root.getFile("backCSS1.txt", null, setValue);
+        fileSystem.root.getFile("backCSS2.txt", null, setValue);
     });    
 }
 function readFont(){
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-        fileSystem.root.getFile("fontCSS1.txt", null, setFF);
+        fileSystem.root.getFile("fontCSS2.txt", null, setFF);
     });    
 }
 function readSize(){
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-        fileSystem.root.getFile("fontsizeCSS1.txt", null, setFontSize);
+        fileSystem.root.getFile("fontsizeCSS2.txt", null, setFontSize);
     });    
 }
 function setValue(fileEntry) {
     fileEntry.file(function(file) {
         var reader = new FileReader();
         reader.onloadend = function(e) {
+            selValue = this.result;
             if(this.result=='Standard'){
                 document.getElementsByClassName("bg1")[0].style.backgroundColor = '#fffff9';  
                 document.getElementsByClassName("bg2")[0].style.backgroundColor = '#fffa8f'; 
@@ -264,8 +268,9 @@ function setFF(fileEntry) {
         var reader = new FileReader();
         reader.onloadend = function(e) {
             document.body.style.fontFamily = this.result;
+            textValue = this.result;
             if (this.result == "'Roboto', sans-serif")
-                document.getElementById("testo").value = "0";
+                document.getElementById("testo").value = "0"; 
             if (this.result == "'OpenDyslexic'")
                 document.getElementById("testo").value = "1";
             if (this.result == "'Chelsea Market', cursive")
@@ -275,7 +280,7 @@ function setFF(fileEntry) {
             if (this.result == "'Open Sans', sans-serif")
                 document.getElementById("testo").value = "4";
             if (this.result == "'Exo', sans-serif")
-                document.getElementById("testo").value = "5";
+                document.getElementById("testo").value = "5";  
             if (this.result == "'Ubuntu', sans-serif")
                 document.getElementById("testo").value = "6";
         }
@@ -288,6 +293,7 @@ function setFontSize(fileEntry) {
         reader.onloadend = function(e) {
             document.body.style.fontSize = this.result;
             document.getElementById("fontsize").value = this.result;
+            sizeValue = this.result;
         }
         reader.readAsText(file);
     });
@@ -297,14 +303,6 @@ function setFontSize(fileEntry) {
 function background(event){
     event.preventDefault();
     back = event.target.value;
-//    if (back == 'Bianco')
-//        back = '#f7f7f7';
-//    if (back == 'Crema')
-//        back = '#FAFAC8';
-//    if (back == 'Giallo')
-//        back = '#FFFF00';
-//    if (back == 'Blu')
-//        back = '#0000FF';
     document.getElementById("testsettings").style.backgroundColor = back;
 };
 
@@ -330,25 +328,11 @@ function ffamily(event){
 function textcolor(event){
     event.preventDefault();
     text = event.target.value;
-//    if (text == 'Bianco')
-//        text = '#f7f7f7';
-//    if (text == 'Nero')
-//        text = '#000000';
-//    if (text == 'Marrone')
-//        text = '#964B00';
-//    if (text == 'Blu')
-//        text = '#0000FF';
     document.getElementById("testsettings").style.color = text;
 };
 function fontsize(event){
     event.preventDefault();
     font = event.target.value;
-//    if (font == 'Piccolo')
-//        font = "small";
-//    if (font == 'Normale')
-//        font = "medium";
-//    if (font == 'Grande')
-//        font = "x-large";
     document.getElementById("testsettings").style.fontSize = font;
 };
 function inter(event){
@@ -359,29 +343,27 @@ function inter(event){
 function wspacing(event){
     event.preventDefault();
     wspc = event.target.value;
-//    if (wspc == 'Nessuna')
-//        wspc = "0px";
     document.getElementById("testsettings").style.wordSpacing = wspc;
 }
 function lspacing(event){
     event.preventDefault();
     lspc = event.target.value;
-//    if (lspc == 'Nessuna')
-//        lspc = "0px";
     document.getElementById("testsettings").style.letterSpacing = lspc;
 }
 // BOTTONE SALVA
 function saveConfiguration(event){
     event.preventDefault();
+    saveAll();
     if (text!=null && back!=null && font!=null && fontCSS!=null && intlinea!= null && wspc!=null && lspc!=null){
         variables =  "0@"+back+"1@"+fontCSS+"2@"+text+"3@"+font+"4@"+intlinea+"5@"+wspc+"6@"+lspc+"7@";
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-            fileSystem.root.getFile("variables.txt", {create: true, exclusive: false}, function(fileEntry){
+            fileSystem.root.getFile("variables1.txt", {create: true, exclusive: false}, function(fileEntry){
                 fileEntry.createWriter(function(writer){
                     writer.write(variables);
                 });
             });
         });
+        
         filecss = "@import url(http://fonts.googleapis.com/css?family=Chelsea+Market);  @import url(https://fonts.googleapis.com/css?family=Slackey); @import url(https://fonts.googleapis.com/css?family=Open+Sans); @import url(https://fonts.googleapis.com/css?family=Exo); @import url(https://fonts.googleapis.com/css?family=Ubuntu); @import url(http://canvasweb.altervista.org/fonts/opendyslexic-regular-webfont.ttf); @font-face{font-family: 'OpenDyslexic'; !important; src: url('http://canvasweb.altervista.org/fonts/opendyslexic-regular-webfont.eot'); src: url('http://canvasweb.altervista.org/fonts/opendyslexic-regular-webfont.eot') format('embedded-opentype'), url('http://canvasweb.altervista.org/fonts/opendyslexic-regular-webfont.woff') format('woff'), url('http://canvasweb.altervista.org/fonts/opendyslexic-regular-webfont.ttf') format('truetype');}          @font-face {font-family: "+fontCSS+" !important; } html, body, div, p, h1, h2, h3, h4, h5, li, ul, ol, table, td, tr, a, list {color: "+text+" !important; text-transform: lowercase !important; text-decoration: none !important; font-family: "+fontCSS+" !important; } html, body, div, p, h1, h2, h3, h4, h5, li, ul, ol, br, table, td, tr, a, list, article, section {background-color: "+back+" !important; } h1, h2, h3, h4, h5 { font-family: "+fontCSS+" !important; } h1{ font-weight: bold !important; font-size: 24px !important; letter-spacing:"+lspc+" !important; word-spacing:"+wspc+" !important;} body {margin: 0px !important; padding: 0px !important;} div, p, article, section { font-family: "+fontCSS+" !important; border: 0px !important; font-size: "+font+"  !important; } p, li { line-height:"+intlinea+" !important;  letter-spacing:"+lspc+" !important; word-spacing:"+wspc+" !important; text-align: left !important; } input, textarea, select{ font-family: "+fontCSS+" !important; background: white !important; border: 1px solid black !important; } form, fieldset {  background: none !important; }";
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
             fileSystem.root.getFile(CSScode, {create: true, exclusive: false}, function(fileEntry){
